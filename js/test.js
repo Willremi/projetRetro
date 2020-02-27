4dd3f2c1e1dbf36f831d72d4b6f20428083d6be1
Willremi <willremi@gmail.com>
2020-02-27 15:19:55 +0100

    test de fontface sur js

--- js/test.js
+++ js/test.js
@@ -1,11 +1,19 @@
 var elt = document.getElementById('annee50');
+var elt1 = document.getElementById('annee60');
 var typewriter = new FontFace('Typewriter', 'url(../fonts/jmh_typewriter_dry/Typewriter-Thin.otf)');
+var alba = new FontFace('alba', 'url(../fonts/alba/alba.ttf)');
 
 typewriter.load().then(function(loaded_face) {
     document.fonts.add(loaded_face);
 }).catch(function(error) {
 
 });
+
+alba.load().then(function(loaded_face) {
+    document.fonts.add(loaded_face);
+}).catch(function(error) {
+
+});
  
 elt.style.fontFamily = "Typewriter";
-
+elt1.style.fontFamily = "alba";
