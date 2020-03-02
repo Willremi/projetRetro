7f8e74f8d3e315ea3b5959f58682a0bbf9669c15
Willremi <willremi@gmail.com>
2020-03-02 13:49:17 +0100

    ajout annecdotes et distinction

--- incorrup.php
+++ incorrup.php
@@ -6,7 +6,7 @@
         src: url("fonts/jmh_typewriter_dry/Typewriter-Thin.otf");
       }
     
-      p, .col-sm-4 ul {
+      p, ul, figcaption {
           font-family: Typewriter;
       }
 
@@ -39,6 +39,7 @@
               <div class="offset-sm-1 col-sm-6 mt-2">
                 <p><strong>Les Incorruptibles</strong> (The Untouchables) est une série télévisée policière américaine en un pilote de 90 minutes et 118 épisodes de 50 minutes, en noir et blanc, créée par Quinn Martin et diffusée entre le 20 avril 1959 et le 21 mai 1963 sur le réseau ABC. Elle y a rencontré un très grand succès : à son apogée, elle était suivie par un ménage américain sur trois.</p>
                 <p>En France, la série a été diffusée à partir du 5 janvier 1964 sur RTF Télévision puis sur la première chaîne de l'ORTF, et au Québec à partir du 8 septembre 1965 à la Télévision de Radio-Canada.</p>
+                <p>Un remake de la série est diffusé entre 1993 - 1994, créé par Christopher Crowe.</p>
                 <hr>
                 <h3>Résumé</h3>
                 <p>Située à Chicago au temps de la prohibition, la série suivait les exploits de l'agent spécial du Trésor Eliot Ness et de son petit groupe d'agents dans leur lutte inlassable contre les membres de la pègre.
@@ -89,13 +90,23 @@
           </div>
         <div class="offset-sm-1">
           <h2>Les images et vidéos</h2>
-          <hr class="ligne">
+          <br>
+          <!-- <hr class="ligne"> -->
           <div id="generique">
           <iframe width="420" height="315"
           src="https://www.youtube.com/embed/ViA0Tuix-50">
           </iframe>
           <p>Générique fait par Nelson Riddle</p>
           <p> Nelson Riddle est un jazzman très en vogue à l'époque qui est appelé aux commandes de la musique. Nelson Riddle signe le thème principal et les musiques des différents épisodes. Douze de celles-ci, ainsi que la générique historique, figurent sur l'album publié en 1960.</p>
+          <figure>
+            <figcaption>La version MP3 du générique :</figcaption>
+            <audio
+           controls
+            src="http://prouvost.com/fun/generiques/--%20Series%20--/Les%20Incorruptibles.mp3" controlsList="nodownload">
+            Votre navigateur ne supporte pas l'élément 
+            <code>audio</code>.
+          </audio>
+          </figure>
           </div>
           <div id="imageSerie">
           <img src="img/incorrup1.jpg" alt="" class="img-thumbnail mr-2">   
@@ -103,11 +114,35 @@
           <img src="img/incorrup3.jpg" alt="" class="img-thumbnail mr-2"> 
           <img src="img/incorrup4.jpg" alt="" class="img-thumbnail">
           </div>
-          <br><br>
+        </div>
           
-        
+       
+        <div class="offset-sm-1">
+        <h2>Annecdotes</h2>
+          <ul class="annecdote" type="circle">
+            <li>Les Incorruptibles n’a pas démarré sur ABC. En effet, le téléfilm original, adapté des écrits d’Eliot Ness en personne et Oscar Fraley, a été proposé en avril 1959 dans le Desilu Playhouse de CBS. Fort du succès du pilote, Quinn Martin, jeune producteur en devenir, s’est vu confier le projet d’une série. Ce dernier deviendra par la suite le créateur du Fugitif, Les Envahisseurs, Cannon ou encore Les Rues de San Francisco.</li>
+            <br>
+            <li>Dans la lignée de la grande tradition du film noir cher au cinéma américain, Les Incorruptibles a été la série la plus violente de la télévision lors de sa diffusion. Pour accentuer son atmosphère, elle a été filmée en grande partie de nuit. La reconstitution du Chicago des années 1930 est très précise, avec les véhicules, les armes, les tenues et les décors. Chaque épisode est introduit en voix off.</li>
+            <br>
+            <li>Elizabeth Ness, la veuve du plus célèbre des Incorruptibles, a déclaré en 1961 : « Je suis très heureuse de l’interprétation de M. Stack. Il a la même voix tranquille et la même gentillesse qui caractérisaient Eliot. Il sourit moins, mais on a donné à M. Stack l’occasion de rire moins souvent qu’à Eliot dans la vie ».</li>
+            <br>
+            <li>Malgré son succès populaire sur sa case du jeudi soir, Les Incorruptibles a essuyé de nombreuses critiques aux États-Unis. La communauté italo-américaine, et en première ligne Frank Sinatra, reprochait aux producteurs de n’utiliser que des patronymes italiens pour les criminels traqués. De son point de vue, J. Edgar Hoover, patron du FBI, n’appréciait pas plus la série, et notamment les libertés prises avec la réalité, dont dans l’arrestation de Ma Barker. Les descendants d’Al Capone ont également exigé la somme d’1 million de dollars de dommages et intérêts pour « exploitation de la notoriété » de leur aïeul...</li>
+            <br>
+            <li>Lors de la saison 4, la série a été édulcorée à la suite des nombreuses critiques. La firme Liggett et Myers, qui parrainait le show, s’est retirée cette même année suite à une menace de boycott.</li>
+            <br>
+            <li>Suite au succès de la série, Robert Stack a accepté de rendosser l’imper d’Eliot Ness en 1991 dans Le Retour d’Eliot Ness. Quelques années plus tôt, en 1987, Kevin Costner avait repris le rôle pour le grand écran, aux côtés de Sean Connery et Robert de Niro, sous la direction de Brian de Palma. En 1993, une série dérivée a été produite. Le Retour des incorruptibles compte 42 épisodes.</li>
+            <br>
+            <li>En France, le pilote a été découvert au cinéma en 1961 sous le titre Le Tueur de Chicago (rebaptisé en 1966 Les Incorruptibles défient Al Capone). La série a, quant à elle, été diffusée sur la deuxième chaîne de l’ORTF dès janvier 1964. Elle a ensuite été à l’antenne de TF1 au cours des années 1980, puis FR3, M6, Série Club ou TV Breizh.</li>
+          </ul>
+          <hr class="ligne">
         </div>
         
+        <div class="offset-sm-4">
+          <h2>Distinctions</h2>
+          <p>Emmy Awards 1960 : Meilleur acteur pour Robert Stack</p>
+
+          
+        </div>
         </div><!-- fin row -->
     </div><!-- fin container-fluid -->
       
