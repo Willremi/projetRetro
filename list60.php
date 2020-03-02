51627010210125257dd9e35b02e8d6dbf7f8d5fb
Willremi <willremi@gmail.com>
2020-03-02 16:25:31 +0100

    ajout page doctor who

--- list60.php
+++ list60.php
@@ -25,7 +25,7 @@
               </thead>
               <tbody>
                 <tr>
-                  <td>Doctor Who (première série)</td>
+                  <td><a href="who.php">Doctor Who (première série)</a></td>
                   <td>Science Fiction</td>
                   <td>Sylvester McCoy, Sophie Aldred</td>
                   <td>Sydney Newman, Donald Wilson</td>
---
+++ who.php
@@ -0,0 +1,230 @@
+<?php require('header.php'); ?>
+  
+  <style>
+      
+    .jumbotron {
+      filter: sepia(.55);
+      }
+      
+    </style>
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12 col-sm-12">
+            <h1 class="mt-2">Doctor Who (première série)</h1>
+            <hr>
+            <div class="row">
+              <div class="offset-sm-2 col-sm-3">
+                <strong>Genres :</strong>
+                <p>Science-Fiction</p>
+              </div>
+              <div class="col-sm-3">
+                <strong>Réalisateurs :</strong> 
+                <p>Sydney Newman, Donald Wilson</p>
+              </div>
+              <div class="col-sm-3">
+                <strong>Années de diffusion : </strong>
+                <p>1963 - 1989</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="offset-sm-1 col-sm-6 mt-2">
+                <p><strong>Doctor Who (La première série)</strong> est une série télévisée britannique de science-fiction créée par Sydney Newman et Donald Wilson et diffusée depuis le 23 novembre 1963 sur BBC One. Elle raconte les aventures du Docteur, qui voyage à travers l'espace et le temps à bord d'un vaisseau spatial, le TARDIS (Time And Relative Dimension In Space, qu'on trouve aussi traduit en version française par « Temps à relativité dimensionnelle inter-spatiale »). 
+                <br>
+                Lorsque le Docteur est mortellement blessé, il peut survivre en se régénérant ; il change alors d'apparence et, dans une certaine mesure, de personnalité, mais tout en conservant le souvenir de ses vies antérieures.
+              <br>
+              Doctor Who est historiquement la plus longue série de science-fiction de tous les temps1,2, comptant 685 épisodes de 26 minutes (dont 253 en noir et blanc), 15 épisodes de 45 minutes, un de 90 minutes et 144 de plus depuis sa re-création de 2005. La série compte donc 846 épisodes et est encore en cours de production.
+              </p>
+                <p>En 1989, après 26 saisons, la BBC décide d'annuler la série. Dans l'espoir de la redémarrer, la chaîne produit avec la FOX un téléfilm en 1996, qui a peu de succès, condamnant le projet. En 2003, la BBC annonce qu'une seconde mouture de la série sera produite par Russell T Davies et Julie Gardner, avec Davies comme scénariste principal. Christopher Eccleston est annoncé comme le nouvel interprète du personnage principal. Diffusée depuis 2005, la seconde série compte à ce jour douze saisons, avec une diffusion dans de nombreux pays dont la France et le Canada.</p>
+                <p>En France, la série est diffusée sous le titre Docteur Who3. Au départ, elle devait être diffusée dans l'émission Temps X. Elle sera finalement diffusée dans le Club Dorothée, à partir du 12 mars 19894 à 9 h sur TF1. Cette dernière a commencé la diffusion de la série avec les aventures du quatrième Docteur (Tom Baker). La seconde série est quant à elle diffusée à partir du 5 novembre 20055 sur France 4. Depuis l'annonce de l'arrêt de la chaîne sur la TNT en 2020, la série est diffusée sur SYFY depuis le 19 mai 20196. Au Canada, elle est diffusée depuis le 4 avril 2005 sur le réseau CBC puis à partir de la cinquième saison en 2009 sur Space. Au Québec, elle est diffusée depuis le 3 janvier 2006 sur Ztélé sous le titre Doctor Who, et disponible sur ICI TOU.TV sous le titre Docteur Who.</p>
+                <hr>
+                <h3>Résumé</h3>
+                <p>Cette série relate les aventures du Docteur, un extraterrestre de la race des Seigneurs du Temps (Time Lord) originaire de la planète Gallifrey. Dans la première série, coupé de son peuple, il voyage à bord d'un TARDIS (Time And Relative Dimension(s) In Space, ou Temps à relativité Dimensionnelle inter Spatiale en français), une machine pouvant voyager dans l'espace et dans le temps. Particulièrement attaché à la Terre, il est régulièrement accompagné dans ses voyages par des compagnons, pour la plupart humains.
+                <br>
+                Le TARDIS a l'apparence d'une cabine téléphonique de police britannique des années 1950, le système de camouflage étant resté bloqué lors d'un accident. En effet, la machine se transforme normalement en temps réel pour ressembler à un élément typique de son environnement.</p>
+              </div>
+              <div class="col-sm-4">
+                <h3>Acteurs principaux</h3>
+                <ul>
+                  <li>William Hartnell : The Doctor</li>
+                  <li>Carole Ann Ford : Susan Foreman</li>
+                  <li>William Russell : Ian Chesterton</li>
+                  <li>Jacqueline Hill : Barbara Wright</li>
+                </ul>
+                <h3>Pays d'origine</h3>
+                <p>Royaume-Uni</p>
+                <h3>Nombre de saisons</h3>
+                <p>26 saisons</p>
+              </div>
+            </div>
+            
+          </div>
+          <div class="offset-sm-1 col-md-10">
+          <table class="table table-bordered rounded saison">
+          <caption>Saisons diffusées dans les années 60</caption>
+          <tr>
+            
+            <td>Saison 1</td>
+            <td>Saison 2</td>
+            <td>Saison 3</td>
+            <td>Saison 4</td>
+            <td>Saison 5</td>
+            <td>Saison 6</td>
+          </tr>
+          <tr>
+            <td>42 épisodes</td>
+            <td>39 épisodes</td>
+            <td>45 épisodes</td>
+            <td>43 épisodes</td>
+            <td>40 épisodes</td>
+            <td>44 épisodes</td>
+          </tr>
+          <tr>
+            <td>1963-1964</td>
+            <td>1964-1965</td>
+            <td>1965-1966</td>
+            <td>1966-1967</td>
+            <td>1967-1968</td>
+            <td>1968-1969</td>
+          </tr>
+        </table>
+          <table class="table table-bordered rounded saison">
+          <caption>Saisons diffusées dans les années 70</caption>
+          <tr>
+            
+            <td>Saison 7</td>
+            <td>Saison 8</td>
+            <td>Saison 9</td>
+            <td>Saison 10</td>
+            <td>Saison 11</td>
+            <td>Saison 12</td>
+            <td>Saison 13</td>
+            <td>Saison 14</td>
+            <td>Saison 15</td>
+            <td>Saison 16</td>
+            <td>Saison 17</td>
+          </tr>
+          <tr>
+            <td>25 épisodes</td>
+            <td>25 épisodes</td>
+            <td>26 épisodes</td>
+            <td>26 épisodes</td>
+            <td>26 épisodes</td>
+            <td>20 épisodes</td>
+            <td>26 épisodes</td>
+            <td>26 épisodes</td>
+            <td>26 épisodes</td>
+            <td>26 épisodes</td>
+            <td>26 épisodes</td>
+          </tr>
+          <tr>
+            <td>1970</td>
+            <td>1971</td>
+            <td>1972</td>
+            <td>1973</td>
+            <td>1974</td>
+            <td>1974-1975</td>
+            <td>1975-1976</td>
+            <td>1976-1977</td>
+            <td>1977-1978</td>
+            <td>1978-1979</td>
+            <td>1979-1980</td>
+          </tr>
+        </table>
+          <table class="table table-bordered rounded saison">
+          <caption>Saisons diffusées dans les années 80</caption>
+          <tr>
+            <td>Saison 18</td>
+            <td>Saison 19</td>
+            <td>Saison 20</td>
+            <td>Saison 21</td>
+            <td>Saison 22</td>
+            <td>Saison 23</td>
+            <td>Saison 24</td>
+            <td>Saison 25</td>
+            <td>Saison 26</td>
+          </tr>
+          <tr>
+            <td>28 épisodes</td>
+            <td>26 épisodes</td>
+            <td>22 épisodes</td>
+            <td>24 épisodes</td>
+            <td>13 épisodes</td>
+            <td>14 épisodes</td>
+            <td>14 épisodes</td>
+            <td>14 épisodes</td>
+            <td>14 épisodes</td>
+            
+          </tr>
+          <tr>
+            <td>1980-1981</td>
+            <td>1982</td>
+            <td>1983</td>
+            <td>1984</td>
+            <td>1985</td>
+            <td>1986</td>
+            <td>1987</td>
+            <td>1988-1989</td>
+            <td>1989</td>
+          </tr>
+        </table>
+          </div>
+        <div class="offset-sm-1">
+          <h2>Les images et vidéos</h2>
+          <br>
+          <!-- <hr class="ligne"> -->
+          <div id="generique">
+          <iframe width="420" height="315"
+          src="https://www.youtube.com/embed/sjDFvoRNpOM">
+          </iframe>
+          <p>Générique composé par l’australien Ron Grainer et réalisé par Delia Derbyshire avec le BBC Radiophonic Workshop et Dick Mills (aucun lien de parenté avec Jeff)</p>
+          <p> La particularité de ce générique, composé en 1963, réside dans le fait qu’il fut entièrement réalisé avec des instruments électroniques et est reconnu comme une pièce importante et innovante de la musique électronique. D’ailleurs, Doctor Who a été la première série télévisée au monde à posséder un thème entièrement réalisé avec ce type d’instruments.</p>
+          <figure>
+            <figcaption>La version MP3 du générique :</figcaption>
+            <audio
+           controls
+            src="media/Doctor Who.mp3" controlsList="nodownload">
+            Votre navigateur ne supporte pas l'élément 
+            <code>audio</code>.
+          </audio>
+          </figure>
+          </div>
+          <div id="imageSerie">
+          <img src="img/incorrup1.jpg" alt="" class="img-thumbnail mr-2">   
+          <img src="img/incorrup2.jpg" alt="" class="img-thumbnail mr-2">   
+          <img src="img/incorrup3.jpg" alt="" class="img-thumbnail mr-2"> 
+          <img src="img/incorrup4.jpg" alt="" class="img-thumbnail">
+          </div>
+        </div>
+          
+       
+        <div class="offset-sm-1">
+        <h2>Annecdotes</h2>
+          <ul class="annecdote" type="circle">
+            <li>Les Incorruptibles n’a pas démarré sur ABC. En effet, le téléfilm original, adapté des écrits d’Eliot Ness en personne et Oscar Fraley, a été proposé en avril 1959 dans le Desilu Playhouse de CBS. Fort du succès du pilote, Quinn Martin, jeune producteur en devenir, s’est vu confier le projet d’une série. Ce dernier deviendra par la suite le créateur du Fugitif, Les Envahisseurs, Cannon ou encore Les Rues de San Francisco.</li>
+            <br>
+            <li>Dans la lignée de la grande tradition du film noir cher au cinéma américain, Les Incorruptibles a été la série la plus violente de la télévision lors de sa diffusion. Pour accentuer son atmosphère, elle a été filmée en grande partie de nuit. La reconstitution du Chicago des années 1930 est très précise, avec les véhicules, les armes, les tenues et les décors. Chaque épisode est introduit en voix off.</li>
+            <br>
+            <li>Elizabeth Ness, la veuve du plus célèbre des Incorruptibles, a déclaré en 1961 : « Je suis très heureuse de l’interprétation de M. Stack. Il a la même voix tranquille et la même gentillesse qui caractérisaient Eliot. Il sourit moins, mais on a donné à M. Stack l’occasion de rire moins souvent qu’à Eliot dans la vie ».</li>
+            <br>
+            <li>Malgré son succès populaire sur sa case du jeudi soir, Les Incorruptibles a essuyé de nombreuses critiques aux États-Unis. La communauté italo-américaine, et en première ligne Frank Sinatra, reprochait aux producteurs de n’utiliser que des patronymes italiens pour les criminels traqués. De son point de vue, J. Edgar Hoover, patron du FBI, n’appréciait pas plus la série, et notamment les libertés prises avec la réalité, dont dans l’arrestation de Ma Barker. Les descendants d’Al Capone ont également exigé la somme d’1 million de dollars de dommages et intérêts pour « exploitation de la notoriété » de leur aïeul...</li>
+            <br>
+            <li>Lors de la saison 4, la série a été édulcorée à la suite des nombreuses critiques. La firme Liggett et Myers, qui parrainait le show, s’est retirée cette même année suite à une menace de boycott.</li>
+            <br>
+            <li>Suite au succès de la série, Robert Stack a accepté de rendosser l’imper d’Eliot Ness en 1991 dans Le Retour d’Eliot Ness. Quelques années plus tôt, en 1987, Kevin Costner avait repris le rôle pour le grand écran, aux côtés de Sean Connery et Robert de Niro, sous la direction de Brian de Palma. En 1993, une série dérivée a été produite. Le Retour des incorruptibles compte 42 épisodes.</li>
+            <br>
+            <li>En France, le pilote a été découvert au cinéma en 1961 sous le titre Le Tueur de Chicago (rebaptisé en 1966 Les Incorruptibles défient Al Capone). La série a, quant à elle, été diffusée sur la deuxième chaîne de l’ORTF dès janvier 1964. Elle a ensuite été à l’antenne de TF1 au cours des années 1980, puis FR3, M6, Série Club ou TV Breizh.</li>
+          </ul>
+          <hr class="ligne">
+        </div>
+        
+        <div class="offset-sm-4">
+          <h2>Distinctions</h2>
+          <p>Emmy Awards 1960 : Meilleur acteur pour Robert Stack</p>
+
+          
+        </div>
+        </div><!-- fin row -->
+    </div><!-- fin container-fluid -->
+      
+<?php require ('footer.php'); ?>
